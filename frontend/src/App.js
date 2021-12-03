3c4ca0911fdb52f180c28e93d1f1ec2f011b1122
Luca Leyva <imlucaleyva@gmail.com>
2021-12-03 16:05:23 -0500

    Setup the Home Screen with Product Offerings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import { Container } from 'react-bootstrap'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen'
 
-const  App = () => {
-  return (
+const App = () => {
+  return(
     <>
     <Header />
     <main className='py-3'>
       <Container>
-        <h1>Welcome to Adapt &amp; Overcome Coaching</h1>
+        <HomeScreen />
       </Container>
     </main>
-    
     <Footer />
     </>
-  );
+  )
 }
-
 export default App;
