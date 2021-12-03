05540b3de1235f7e1309a82370b3fbf292931e35
Luca Leyva <imlucaleyva@gmail.com>
2021-12-03 16:19:03 -0500

    Added Rating component and made visible from the product cards on the Home Screen.

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = ({product}) => {
     return (
-        <Card>
-            <a href={`/product/${product._id}`}>
+        <Card className='my-3 p-3 rounded' style={{minHeight:'430px'}}>
+            <a href={`/product/${product._id}`}> 
                 <Card.Img src={product.image} varian='top' />
             </a>
 
@@ -14,9 +15,7 @@ const Product = ({product}) => {
                 </a>
 
                 <Card.Text as='div'>
-                    <div className='my-3'>
-                        {product.rating} from {product.numReviews} reviews
-                    </div>
+                    <Rating value={product.rating} text={`${product.numReviews} reviews`} />
                 </Card.Text>
 
                 <Card.Text as='h3'>&#36;{product.price}</Card.Text>
